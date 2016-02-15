072a4245d0c4475c8a586d754521208183fc796a
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-15 20:06:06 -0200

    it starts

---
+++ Xcode/System/System.h
@@ -0,0 +1,19 @@
+//
+//  System.h
+//  System
+//
+//  Created by David Ask on 15/02/16.
+//  Copyright © 2016 Zewo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for System.
+FOUNDATION_EXPORT double SystemVersionNumber;
+
+//! Project version string for System.
+FOUNDATION_EXPORT const unsigned char SystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <System/PublicHeader.h>
+
+

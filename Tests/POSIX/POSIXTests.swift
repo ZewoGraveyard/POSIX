f39977a09ae006930ab04412da9205612fb72741
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-25 01:36:12 -0300

    add tests

---
+++ Tests/POSIX/POSIXTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import POSIX
+
+class POSIXTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension POSIXTests {
+    static var allTests : [(String, POSIXTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
